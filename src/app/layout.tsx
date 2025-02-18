a3429a4dac895dba97284f32dcd09de286906eeb
nakmiers <diwas118151@gmail.com>
2025-02-18 20:52:47 +0700

    OK

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,15 @@ import { Toaster } from 'react-hot-toast'
 import './globals.scss'
 
 export const metadata: Metadata = {
-  title: 'Personal Expenses Management',
+  title: 'PEM ',
   description: 'Personal Expenses Management Web Application',
+  keywords: 'pem',
+  icons: {
+    icon: ['/favicon.ico?v=4'],
+    apple: ['/apple-touch-icon.png?v=4'],
+    shortcut: ['/apple-touch-icon.png'],
+  },
+  manifest: '/site.webmanifest',
 }
 
 export default function RootLayout({
@@ -18,7 +25,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html
         lang="en"
-        className="text-light bg-neutral-900"
+        className="bg-neutral-900 text-light"
       >
         <body>
           <Toaster position="bottom-left" />
