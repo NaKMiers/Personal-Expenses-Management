1ec357294d0852dc8c54f37c0246c0a508148003
nakmiers <diwas118151@gmail.com>
2025-03-31 13:18:54 +0700

    fixed small bug

--- src/app/api/budgets/[id]/edit/route.ts
+++ src/app/api/budgets/[id]/edit/route.ts
@@ -1,35 +1,38 @@
-import { connectDatabase } from '@/config/database';
-import { NextRequest, NextResponse } from 'next/server';
+import { connectDatabase } from '@/config/database'
+import { NextRequest, NextResponse } from 'next/server'
 import Budget from '@/models/BudgetModel'
 
 // [PUT]: /api/budgets/[id]
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+export async function PUT(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
   try {
     // Kết nối database
-    await connectDatabase();
+    await connectDatabase()
 
     // Lấy ID từ params
-    const { id } = params;
+    const { id } = await params
 
     // Lấy dữ liệu từ request body
-    const updateData = await req.json();
+    const updateData = await req.json()
 
     // Tìm và cập nhật ngân sách
     const updatedBudget = await Budget.findByIdAndUpdate(
       id,
       updateData, // Dữ liệu cần cập nhật
       { new: true } // Trả về bản ghi đã được cập nhật
-    );
+    )
 
     // Kiểm tra nếu ngân sách không tồn tại
     if (!updatedBudget) {
-      return NextResponse.json({ message: 'Budget not found' }, { status: 404 });
+      return NextResponse.json({ message: 'Budget not found' }, { status: 404 })
     }
 
     // Trả về response
-    return NextResponse.json({ budget: updatedBudget, message: 'Budget updated successfully' }, { status: 200 });
+    return NextResponse.json(
+      { budget: updatedBudget, message: 'Budget updated successfully' },
+      { status: 200 }
+    )
   } catch (err: any) {
     // Xử lý lỗi
-    return NextResponse.json({ message: err.message }, { status: 500 });
+    return NextResponse.json({ message: err.message }, { status: 500 })
   }
 }
