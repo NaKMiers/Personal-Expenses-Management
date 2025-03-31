7f8304ad14ef20fcad76dfadb6008b22f066cbb8
nakmiers <diwas118151@gmail.com>
2025-03-31 13:16:20 +0700

    fixed small bug

--- src/app/api/budgets/[id]/delete/route.ts
+++ src/app/api/budgets/[id]/delete/route.ts
@@ -1,28 +1,27 @@
-import { NextRequest, NextResponse } from 'next/server';
-import Budget from '@/models/BudgetModel'
 import { connectDatabase } from '@/config/database'
+import Budget from '@/models/BudgetModel'
+import { NextRequest, NextResponse } from 'next/server'
 
 // [DELETE]: /api/budgets/[id]
-export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+export async function DELETE(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
   try {
-    // Kết nối database
-    await connectDatabase();
+    await connectDatabase()
 
     // Lấy ID từ params
-    const { id } = params;
+    const { id } = await params
 
     // Tìm và xóa ngân sách
-    const deletedBudget = await Budget.findByIdAndDelete(id);
+    const deletedBudget = await Budget.findByIdAndDelete(id)
 
     // Kiểm tra nếu ngân sách không tồn tại
     if (!deletedBudget) {
-      return NextResponse.json({ message: 'Budget not found' }, { status: 404 });
+      return NextResponse.json({ message: 'Budget not found' }, { status: 404 })
     }
 
     // Trả về response
-    return NextResponse.json({ message: 'Budget deleted successfully' }, { status: 200 });
+    return NextResponse.json({ message: 'Budget deleted successfully' }, { status: 200 })
   } catch (err: any) {
     // Xử lý lỗi
-    return NextResponse.json({ message: err.message }, { status: 500 });
+    return NextResponse.json({ message: err.message }, { status: 500 })
   }
 }
